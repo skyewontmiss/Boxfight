05c37ed775add15ac263e8c95dbaba4c37a95d76
BossCreeper48 <bosscreeper24gamer@gmail.com>
2021-12-28 17:31:59 +0200

    NavMeshAganet enemy script

---
+++ Source Code/Assets/Scripts/LocalScripts/NavMeshAgentEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentEnemy : MonoBehaviour
+{
+    //add NavMeshAgent component to the enemy and bake the mesh so it can follow you
+    //easy enemy 
+    public GameObject player;
+    private NavMeshAgent enemy;
+    public float distanceToAtack;
+    public float nowdistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination = player.transform.position;
+        nowdistance = Vector3.Distance(player.transform.position, this.gameObject.transform.position);
+        if(distanceToAtack <= nowdistance)
+        {
+            Debug.Log("TOO CLOSE ATACK");
+        }
+    }
+}
